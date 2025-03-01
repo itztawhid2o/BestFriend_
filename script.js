d73b2aaff0b65cb62abdfe45c80cc66d3cd8ec63
itztawhid2o <ripon9988a@gmail.com>
2025-03-01 15:17:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey Bestie! You're the most adorable human ever! 💖";
+const greetingText = "Hey Puchki🤏🏻! It'z Nobita'z Style! 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
